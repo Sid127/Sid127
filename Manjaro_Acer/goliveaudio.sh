05f09c227f4be3c1c3f74f4e314d3113ce3282ad
Sid <sidpranjale127@protonmail.com>
2021-06-22 16:23:03 +0530

    Popcorn: Buttered r1
    Signed-off-by: Sid <sidpranjale127@protonmail.com>

--- Manjaro_Acer/goliveaudio.sh
+++
@@ -1,6 +0,0 @@
-# Pulse config for streaming
-pactl load-module module-null-sink sink_name=Combined_Output sink_properties=device.description=Combined_Output
-pactl load-module module-null-sink sink_name=Recorded_Sink sink_properties=device.description=Recorded_Sink
-pactl load-module module-loopback source=alsa_input.pci-0000_00_1f.3.analog-stereo sink=Combined_Output
-pactl load-module module-loopback source=3 sink=Combined_Output
-pactl load-module module-loopback source=3 sink=alsa_output.pci-0000_00_1f.3.analog-stereo
--- popcorn.md
+++ popcorn.md
@@ -1,3 +1,5 @@
-- Enable USB Fast Charge
-- Add State Notifier
-- Minor GPU optimization
+I'm back after a long hiatus (mainly due to fucking entrances) with a new release of Popcorn Kernel!
+- Rebased to fresh Enigma sources
+- CAF tag LA.UM.9.2.r1-03400-SDMxx0.0 
+- Dropped all previous popcorn changes (they'll be back in a future release)
+- Fixed VoIP and (hopefully) Camera on PL2
